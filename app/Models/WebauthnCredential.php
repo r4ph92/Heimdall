719dd9214ecc23ab633af227cfbe7917317ed0e5
r4ph92 <raphael.houle03@gmail.com>
2026-05-08 20:27:20 -0400

    fix: add user() relationship to WebauthnCredential model

--- app/Models/WebauthnCredential.php
+++ app/Models/WebauthnCredential.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WebauthnCredential extends Model
 {
@@ -20,5 +21,10 @@ class WebauthnCredential extends Model
         return [
             'last_used_at' => 'datetime',
         ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
